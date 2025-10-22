bf6a18da36b338fd06735cb75eaae9eb546dd6fb
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-10-22 19:10:23 -0300

    Corrigido alguns inputs na tela de cadastro.

--- src/app/pages/imoveis/list.ts
+++ src/app/pages/imoveis/list.ts
@@ -4,6 +4,12 @@ import { ButtonModule } from 'primeng/button';
 import { ImageModule } from 'primeng/image';
 import { TagModule } from 'primeng/tag';
 
+export interface ImovelResponse {
+  nome: string;
+  valor: number;
+  caracteristicas: string[]
+}
+
 @Component({
   selector: 'app-list',
   imports: [
@@ -22,7 +28,9 @@ import { TagModule } from 'primeng/tag';
     </div>
 
      <!-- Para fazer o modelo de listagem -->
+      @for(imovel of imoveis; track imovel) {
     <div class="bg-gray-900 border-round mt-3 pb-2 pl-2 w-full">
+
       <div class="flex flex-row w-full">
       <div class="flex justify-between w-full">
         <div class="border-r-2 mt-2 mr-3 p-4 pl-2">
@@ -34,20 +42,17 @@ import { TagModule } from 'primeng/tag';
             class="max-h-min mt-2"
             value="Codigo Imóvel" 
             [rounded]="true"/>
-            <h3><strong class="m-0 text-2x1">Nome Imóvel</strong></h3>
+            <h3><strong class="m-0 text-2x1">{{imovel.nome}}</strong></h3>
           </div>
         <div><h5>Localização Imóvel</h5></div>
-        <div><h2><strong class="text-primary">Valor Imóvel</strong></h2></div>
+        <div><h2><strong class="text-primary">R$ {{imovel.valor}}</strong></h2></div>
         <div class="flex gap-5 flex-wrap flex-row">
-          <p-tag 
-          value="caracteristicas 1"
-          severity="secondary" />
-          <p-tag 
-          value="caracteristicas 2"
-          severity="secondary" />
-          <p-tag 
-          value="caracteristicas 3"
-          severity="secondary" />
+          @for (caracteristica of imovel.caracteristicas; track caracteristica) {
+
+            <p-tag 
+            value="{{caracteristica}}"
+            severity="secondary" />
+          } 
         </div>
       </div>
       <div class="flex border-l-2 mt-2 mr-3 items-center">
@@ -69,8 +74,24 @@ import { TagModule } from 'primeng/tag';
     </div>
   </div>
 </div>
+      }
   `,
   styles: ``
 })
 export class ImovelList {
+  imoveis:ImovelResponse[];
+  constructor() {
+    this.imoveis = [
+      {
+        nome: "Residencial Batatinha",
+        valor: 1200000,
+        caracteristicas: ["2 quartos", "1 banheiro"]
+      },
+      {
+        nome: "Residencial CAP",
+        valor: 100000,
+        caracteristicas: ["2 quartos", "1 suíte", "1 vaga"]
+      }
+    ]
+  }
 }
