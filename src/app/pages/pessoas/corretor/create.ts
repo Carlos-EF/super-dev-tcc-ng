cbc9fa7b27008cbdad56e7124dd02e743f074541
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-10-24 11:10:36 -0300

    Criado página de cadastro de corretor e feito navegação entre ela e listagem de pessoas.

--- src/app/pages/pessoas/corretor/create.ts
+++ src/app/pages/pessoas/corretor/create.ts
@@ -1,15 +1,90 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DatePickerModule } from 'primeng/datepicker';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [
+    InputMaskModule,
+    InputTextModule,
+    InputNumberModule,
+    ButtonModule,
+    DatePickerModule,
+  ],
   template: `
-    <p>
-      create works!
-    </p>
+  <div>
+    <div class="card flex flex-col p-2 gap-3">
+      <div class="flex flex-col basis-0 gap-2">
+        <label for="">Nome Completo:</label>
+        <input type="text" pInputText class="p-component p-inputtext">
+      </div>
+      
+      <div class="flex flex-wrap gap-6">
+        <div class="flex flex-col basis-0 gap-2">
+          <label for="">Celular:</label>
+          <p-inputmask mask="(99) 99999-9999" placeholder="(00) 00000-0000" />
+        </div>
+          
+        <div class="flex flex-col basis-0 grow gap-2">
+          <label for="">Email:</label>
+          <input type="email" pInputText>
+        </div>
+
+        <div class="flex flex-col basis-0 gap-2">
+          <label for="">CRECI:</label>
+          <p-inputnumber [maxlength]="5"/>
+        </div>
+      </div>
+    </div>
+
+    <div class="card flex flex-col p-2 gap-3">
+    <div class="font-bold text-xl">Informações Pessoais: <span class="text-sm text-surface-500">(Opcional)</span></div>
+
+    <!-- Ajustar no futuro o layout desta parte do cadastro -->
+    <div class="flex flex-wrap justify-between gap-6">
+      <div class="flex flex-col basis-0 gap-2">
+        <label for="">RG:</label>
+        <p-input-mask mask="9.999.999" placeholder="0.000.000" />
+      </div>
+
+        <div class="flex flex-col basis-0 gap-2">
+          <label for="">CPF:</label>
+          <p-inputmask mask="999.999.999-99" placeholder="000.000.000-00" />
+        </div>
+          
+        <div class="flex flex-col grow gap-2">
+          <label for="">Data de Nascimento:</label>
+          <p-date-picker dateFormat="dd/mm/yy" />
+        </div>
+
+        <div class="flex flex-col grow gap-2">
+          <label for="">Cadastrado desde:</label>
+          <p-date-picker dateFormat="dd/mm/yy" />
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-end">
+      <p-button
+      icon="pi pi-save"
+      label="Salvar"
+      severity="success"
+      (click)="salvar()" />
+    </div>
+  </div>
+  <!-- Ajustar no futuro o layout desta parte do cadastro -->
   `,
   styles: ``
 })
 export class CorretorCreate {
-
+  constructor(
+    private router: Router
+  ) { }
+salvar() {
+  this.router.navigate(["/pages/pessoas"])
+}
 }
