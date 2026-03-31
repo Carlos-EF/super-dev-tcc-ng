2329410d402546e395eebc607d126d3630d789ab
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-03-31 09:11:54 -0300

    Criado componente de botões para ativar e inativar.

---
+++ src/app/layout/component/action buttons/status-button.ts
@@ -0,0 +1,29 @@
+import { Component, input } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'status-button',
+  imports: [ButtonModule],
+  template: `
+  @if (status() ==  "ATIVO") {
+    <div class="flex flex-col justify-between gap-4">
+      <p-button
+      icon="pi pi-circle-off"
+      severity="warn"
+      [outlined]="true"
+      />
+    </div>
+  } @else {
+    <div class="flex flex-col justify-between gap-4">
+      <p-button
+      icon="pi pi-circle-on"
+      severity="success"
+      [outlined]="true"
+      />
+    </div>
+  }
+  `
+})
+export class StatusButton {
+  status = input<string>();
+}
