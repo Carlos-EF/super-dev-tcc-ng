737778e36411af7dfea41f478e5697fe954b2e6d
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-04-10 08:54:40 -0300

    Criado modelo de criar corretor.

--- src/app/models/corretor.model.ts
+++ src/app/models/corretor.model.ts
@@ -10,3 +10,14 @@ export interface CorretorResponse {
     rg: string;
     cpf: string;
 }
+
+export interface CorretorCriarRequest {
+    nome_completo: string;
+    codigo: string;
+    celular: string;
+    email: string;
+    creci: string;
+    data_nascimento: string;
+    rg: string;
+    cpf: string;
+}
