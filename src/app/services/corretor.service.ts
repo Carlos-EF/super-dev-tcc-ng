e74f35289f458ef262a8d6c3da43ebefa182ece0
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-04-10 09:01:53 -0300

    Criado getAll de corretor.

--- src/app/services/corretor.service.ts
+++ src/app/services/corretor.service.ts
@@ -1,10 +1,18 @@
+import { CorretorResponse } from '@/models/corretor.model';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { enviroment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CorretorService {
   private httpClient = inject(HttpClient);
+
+  getAll(): Observable<CorretorResponse[]> {
+    const url = `${enviroment.apiUrl}/corretores`;
+
+    return this.httpClient.get<CorretorResponse[]>(url);
+  }
 }
