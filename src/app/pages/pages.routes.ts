d958f3a0379ecd4076826df8f897e928fb7d4dba
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-04-14 08:57:15 -0300

    Deletado edit.ts de corretor; corrigido validators do cadastro de corretor.

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -4,14 +4,12 @@ import { ImovelCreate } from './imoveis/create';
 import { PessoasList } from './pessoas/list';
 import { CorretorCreate } from './pessoas/corretor/create';
 import { ClienteCreate } from './pessoas/cliente/create';
-import { CorretorEdit } from './pessoas/corretor/edit';
 
 export default [
     { path: 'imoveis', component: ImovelList},
     { path: 'imoveis/cadastrar', component: ImovelCreate},
     { path: 'pessoas', component: PessoasList},
     { path: 'pessoas/corretor/cadastrar', component: CorretorCreate},
-    { path: 'pessoas/corretor/editar/:id', component: CorretorEdit},
     { path: 'pessoas/cliente/cadastrar', component: ClienteCreate},
     { path: '**', redirectTo: '/notfound' }
 ] as Routes;
