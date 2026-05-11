8fcb379198e03956b664a55027178678f49d49aa
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-05-11 07:51:36 -0300

    Criado models de clientes do tipo interessado.

--- src/app/models/cliente.model.ts
+++ src/app/models/cliente.model.ts
@@ -25,3 +25,42 @@ export interface ClienteEditarRequest {
     tipo: string;
     dados_adicionais: EditarClienteInteressadoRequest | EditarClienteProprietarioRequest | EditarClienteLocatarioRequest;
 }
+
+export interface ClienteInteressadoResponse {
+    procurando: string;
+    orcamento: number;
+    orcamento_minimo: number;
+    orcamento_maximo: number;
+    quantidade_quartos: number;
+    quantidade_suites: number;
+    quantidade_banheiros: number;
+    quantidade_vagas_garagem: number;
+    quantidade_andares: number;
+    quantidade_salas: number;
+}
+
+export interface EditarClienteInteressadoRequest {
+    procurando: string;
+    orcamento: number;
+    orcamento_minimo: number;
+    orcamento_maximo: number;
+    quantidade_quartos: number;
+    quantidade_suites: number;
+    quantidade_banheiros: number;
+    quantidade_vagas_garagem: number;
+    quantidade_andares: number;
+    quantidade_salas: number;
+}
+
+export interface CriarClienteInteressadoRequest {
+    procurando: string;
+    orcamento: number;
+    orcamento_minimo: number;
+    orcamento_maximo: number;
+    quantidade_quartos: number;
+    quantidade_suites: number;
+    quantidade_banheiros: number;
+    quantidade_vagas_garagem: number;
+    quantidade_andares: number;
+    quantidade_salas: number;
+}
