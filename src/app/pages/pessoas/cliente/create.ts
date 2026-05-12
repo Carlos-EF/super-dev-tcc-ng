4cf8e6b29a8b68a04c081cafe35356134c5b9606
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-05-12 09:18:33 -0300

    Adicionado form de dados adicionais e seus controlNames.

--- src/app/pages/pessoas/cliente/create.ts
+++ src/app/pages/pessoas/cliente/create.ts
@@ -40,33 +40,32 @@ export interface TiposImoveis {
   ],
   template: `
   <form [formGroup]="clienteForm">
-
     <div class="card flex flex-col gap-4">
       <div class="font-bold text-xl">Cadastro de Cliente:</div>
       <div class="flex flex-wrap basis-0 gap-3">
         <div class="flex flex-col grow gap-2">
           <label for="">Nome Completo: <span class="text-red-500"><strong> *</strong></span></label>
-          <input type="text" pInputText placeholder="Digite o nome do cliente.">
+          <input type="text" pInputText placeholder="Digite o nome do cliente." formControlName="nome" />
         </div>
         
         <div class="flex flex-col grow gap-2">
           <label for="campo-codigo">Código: <span class="text-red-500"><strong> *</strong></span></label>
-          <input pInputText id="campo-codigo" type="text" placeholder="Digite o código de referência do cliente." />
+          <input pInputText id="campo-codigo" type="text" placeholder="Digite o código de referência do cliente." formControlName="codigo" />
         </div>
         
         <div class="flex flex-col basis-0 gap-2">
           <label for="">Celular: <span class="text-red-500"><strong> *</strong></span></label>
-          <p-inputmask mask="(99) 99999-9999" placeholder="(00) 00000-0000" />
+          <p-inputmask mask="(99) 99999-9999" placeholder="(00) 00000-0000" formControlName="celular" />
         </div>
         
         <div class="flex flex-col grow basis-0 gap-2">
           <label for="">E-mail: <span class="text-red-500"><strong> *</strong></span></label>
-          <input type="email" pInputText placeholder="Digite o e-mail do cliente.">
+          <input type="email" pInputText placeholder="Digite o e-mail do cliente." formControlName="email">
         </div>
         
         <div class="flex flex-col grow basis-0 gap-2">
           <label for="">Como nos encontrou:</label>
-          <p-select [options]="opcoesContato" optionLabel="opcao" optionValue="opcao" placeholder="Selecione por onde o cliente veio."/>
+          <p-select [options]="opcoesContato" optionLabel="opcao" optionValue="opcao" placeholder="Selecione por onde o cliente veio." formControlName="como_encontrou"/>
         </div>
 
       <div class="flex flex-col grow basis-0 gap-2">
@@ -78,137 +77,144 @@ export interface TiposImoveis {
     <div id="mostrar-form" class="flex flex-col gap-4">
       @switch (clienteForm.get("tipo")?.value) {
         @case ("Interessado") {
-          <div id="form-interessado" class="card flex flex-col gap-4">
-            <div class="font-bold text-xl">Informações Sobre o Cliente:</div>
-            
-            <div class="flex flex-col gap-2">
-              <label for="">O que está Procurando? <span class="text-red-500"><strong> *</strong></span></label>
-              <p-select [options]="tipoImovel" optionLabel="tipo" optionValue="tipo" placeholder="Selecione a opção desejada." [(ngModel)]="tipoSelecionado"/>
-            </div>
-            
-            <div class="text-xl font-bold">Valores:</div>
-            <div class="flex flex-wrap gap-2">
+          <form formGroupName="dados_adicionais">
+
+            <div id="form-interessado" class="card flex flex-col gap-4">
+              <div class="font-bold text-xl">Informações Sobre o Cliente:</div>
               
-              <div class="flex flex-col grow gap-2">
-                <label for="">Orçamento: <span class="text-red-500"><strong> *</strong></span></label>
-                <p-inputnumber mode="currency" placeholder="Orçamento do cliente." currency="BRL" locale="pt-BR" />
+              <div class="flex flex-col gap-2">
+                <label for="">O que está Procurando? <span class="text-red-500"><strong> *</strong></span></label>
+                <p-select [options]="tipoImovel" optionLabel="tipo" optionValue="tipo" placeholder="Selecione a opção desejada." formControlName="tipo_imovel"/>
               </div>
               
-              <div class="flex flex-col grow gap-2">
-                <label for="">Orçamento Mínimo:</label>
-                <p-inputnumber mode="currency" placeholder="Valor mínimo do orçamento do cliente." currency="BRL" locale="pt-BR" />
+              <div class="text-xl font-bold">Valores:</div>
+              <div class="flex flex-wrap gap-2">
+                
+                <div class="flex flex-col grow gap-2">
+                  <label for="">Orçamento: <span class="text-red-500"><strong> *</strong></span></label>
+                  <p-inputnumber mode="currency" placeholder="Orçamento do cliente." currency="BRL" locale="pt-BR" formControlName="orcamento" />
+                </div>
+                
+                <div class="flex flex-col grow gap-2">
+                  <label for="">Orçamento Mínimo:</label>
+                  <p-inputnumber mode="currency" placeholder="Valor mínimo do orçamento do cliente." currency="BRL" locale="pt-BR" formControlName="orcamento_minimo"/>
+                </div>
+                
+                <div class="flex flex-col grow gap-2">
+                  <label for="">Orçamento Máximo:</label>
+                  <p-inputnumber mode="currency" placeholder="Valor máximo do orçamento do cliente." currency="BRL" locale="pt-BR" formControlName="orcamento_maximo"/>
+                </div>
               </div>
               
-              <div class="flex flex-col grow gap-2">
-                <label for="">Orçamento Máximo:</label>
-                <p-inputnumber mode="currency" placeholder="Valor máximo do orçamento do cliente." currency="BRL" locale="pt-BR" />
-              </div>
-            </div>
-            
-            <div class="flex flex-col gap-2">
-              <div class="font-bold text-xl">Preferências:</div>
-              
-              <div class="flex flex-wrap gap-6">
-                <div class="flex flex-col grow basis-0 gap-2">
-                  <label for="campo-quartos">Quantidade de Quartos:</label>
-                  <p-inputnumber
-                  id="campo-quartos" 
-                  placeholder="Digite a quantidade de quartos." />
-                </div>
+              <div class="flex flex-col gap-2">
+                <div class="font-bold text-xl">Preferências:</div>
                 
-                <div class="flex flex-col grow basis-0 gap-2">
-                  <label for="campo-suites">Sendo Suítes:</label>
-                  <p-inputnumber
-                  id="campo-suites"
-                  placeholder="Digite a quantidade de suítes." />
-                </div>
-                
-                <div class="flex flex-col grow basis-0 gap-2">
-                  <label for="campo-banheiro">Quantidade de Banheiros:</label>
-                  <p-inputnumber
-                  id="campo-banheiro" 
-                  placeholder="Digite a quantidade de banheiros." />
-                </div>
-                
-                <div class="flex flex-col grow basis-0 gap-2">
-                  <label for="campo-vagas">Vagas de Garagem:</label>
-                  <p-inputnumber 
+                <div class="flex flex-wrap gap-6">
+                  <div class="flex flex-col grow basis-0 gap-2">
+                    <label for="campo-quartos">Quantidade de Quartos:</label>
+                    <p-inputnumber
+                    id="campo-quartos" 
+                    placeholder="Digite a quantidade de quartos." formControlName="quantidade_quartos"/>
+                  </div>
+                  
+                  <div class="flex flex-col grow basis-0 gap-2">
+                    <label for="campo-suites">Sendo Suítes:</label>
+                    <p-inputnumber
+                    id="campo-suites"
+                    placeholder="Digite a quantidade de suítes." formControlName="quantidade_suites"/>
+                  </div>
+                  
+                  <div class="flex flex-col grow basis-0 gap-2">
+                    <label for="campo-banheiro">Quantidade de Banheiros:</label>
+                    <p-inputnumber
+                    id="campo-banheiro" 
+                    placeholder="Digite a quantidade de banheiros." formControlName="quantidade_banheiros"/>
+                  </div>
+                  
+                  <div class="flex flex-col grow basis-0 gap-2">
+                    <label for="campo-vagas">Vagas de Garagem:</label>
+                    <p-inputnumber 
                   id="campo-vagas"
-                  placeholder="Digite a quantidade de vagas de garagem." />
+                  placeholder="Digite a quantidade de vagas de garagem." formControlName="quantidade_vagas"/>
                 </div>
                 
                 <div class="flex flex-col grow basis-0 gap-2">
                   <label for="campo-andares">Andares:</label>
-          <p-inputnumber 
-          id="campo-andares"
-          placeholder="Digite a quantidade de andares." />
-        </div>
-        
-        <div class="flex flex-col grow basis-0 gap-2">
-          <label for="campo-salas">Quantidade de Salas:</label>
-          <p-inputnumber
-          id="campo-salas" 
-          placeholder="Digite a quantidade de salas." />
-        </div>
-      </div>
-    </div>
-  </div>
-  
-  <div class="flex justify-end">
-    <p-button
-    severity="success"
-    icon="pi pi-save"
-    label="Salvar"
-    (click)="salvar()" />
-  </div>
-}
-@case ("Proprietário") {
-  <div class="flex card gap-2 flex-col">
-    <div class="flex flex-col grow basis-0 gap-4">
-      <div class="text-xl font-bold">Informações Adicionais:</div>
-      
-      <label for="">Imóvel do Responsável:</label>
-      <div class="flex flex-row">
-        <p-select [options]="imoveis" optionLabel="nome" optionValue="nome" placeholder="Selecione o imóvel." fluid />
-        <p-button
-        severity="primary"
-        icon="pi pi-plus" />
-      </div>
-    </div>
-  </div>
-  
-    <div class="flex justify-end">
-      <p-button
-      severity="success"
-      icon="pi pi-save"
-      label="Salvar"
-      (click)="salvar()" />
-    </div>
-          }
-          @case ("Locatário") {
-            <div class="flex card gap-2 flex-col">
-              <div class="flex flex-col grow basis-0 gap-4">
-                <div class="text-xl font-bold">Informações Adicionais:</div>
+                  <p-inputnumber 
+                  id="campo-andares"
+                  placeholder="Digite a quantidade de andares." formControlName="quantidade_andares"/>
+                </div>
                 
+                <div class="flex flex-col grow basis-0 gap-2">
+                  <label for="campo-salas">Quantidade de Salas:</label>
+                  <p-inputnumber
+                  id="campo-salas" 
+                  placeholder="Digite a quantidade de salas." formControlName="quantidade_salas"/>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="flex justify-end">
+            <p-button
+            severity="success"
+            icon="pi pi-save"
+            label="Salvar"
+            (click)="salvar()" />
+          </div>
+      </form>
+       }
+        @case ("Proprietário") {
+          <form formGroupName="dados_adicionais">
+          <div class="flex card gap-2 flex-col">
+            <div class="flex flex-col grow basis-0 gap-4">
+              <div class="text-xl font-bold">Informações Adicionais:</div>
+              
+              <label for="">Imóvel do Responsável:</label>
+              <div class="flex flex-row">
+                <p-select [options]="imoveis" optionLabel="nome" optionValue="nome" placeholder="Selecione o imóvel." fluid formControlName="imovel_associado" />
+                <p-button
+                severity="primary"
+                icon="pi pi-plus" />
+              </div>
+            </div>
+          </div>
+          
+            <div class="flex justify-end">
+              <p-button
+              severity="success"
+              icon="pi pi-save"
+              label="Salvar"
+              (click)="salvar()" />
+            </div>
+          </form>
+      }
+        @case ("Locatário") {
+        <form formGroupName="dados_adicionais">
+          <div class="flex card gap-2 flex-col">
+            <div class="flex flex-col grow basis-0 gap-4">
+            <div class="text-xl font-bold">Informações Adicionais:</div>
+              
           <label for="">Imóvel do Responsável:</label>
           <div class="flex flex-row">
-            <p-select [options]="imoveis" optionLabel="nome" optionValue="nome" placeholder="Selecione o imóvel." fluid />
+            <p-select [options]="imoveis" optionLabel="nome" optionValue="nome" placeholder="Selecione o imóvel." fluid formControlName="imovel_associado" />
             <p-button
             severity="primary"
             icon="pi pi-plus" />
             </div>
           </div>
         </div>
-        
-        <div class="flex justify-end">
-          <p-button
-          severity="success"
-          icon="pi pi-save"
-          label="Salvar"
-          (click)="salvar()" />
-        </div>
+      
+      <div class="flex justify-end">
+        <p-button
+        severity="success"
+        icon="pi pi-save"
+        label="Salvar"
+        (click)="salvar()" />
+      </div>
+    </form>
       }
-      @default {
+        @default {
       }
     }
     </div>
