0c27639ec2f7135c54da6784cfaa0239a6c81cfa
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-05-12 08:38:55 -0300

    Criado pasta de types; criado type de clientes; começado a adicionar formGroup no cadastro de cliente.

--- src/app/pages/pessoas/cliente/create.ts
+++ src/app/pages/pessoas/cliente/create.ts
@@ -1,6 +1,7 @@
+import { ClienteService } from '@/services/cliente.service';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
@@ -39,109 +40,111 @@ export interface TiposImoveis {
     CommonModule,
   ],
   template: `
-  <div class="card flex flex-col gap-4">
-    <div class="font-bold text-xl">Cadastro de Cliente:</div>
-    <div class="flex flex-wrap basis-0 gap-3">
-      <div class="flex flex-col grow gap-2">
-        <label for="">Nome Completo: <span class="text-red-500"><strong> *</strong></span></label>
-        <input type="text" pInputText placeholder="Digite o nome do cliente.">
-      </div>
+  <form [formGroup]="clienteForm">
 
-      <div class="flex flex-col grow gap-2">
-        <label for="campo-codigo">Código: <span class="text-red-500"><strong> *</strong></span></label>
-        <input pInputText id="campo-codigo" type="text" placeholder="Digite o código de referência do cliente." />
-      </div>
-    
-      <div class="flex flex-col basis-0 gap-2">
-        <label for="">Celular: <span class="text-red-500"><strong> *</strong></span></label>
-        <p-inputmask mask="(99) 99999-9999" placeholder="(00) 00000-0000" />
-      </div>
-          
-      <div class="flex flex-col grow basis-0 gap-2">
-        <label for="">E-mail: <span class="text-red-500"><strong> *</strong></span></label>
-        <input type="email" pInputText placeholder="Digite o e-mail do cliente.">
-      </div>
-
-      <div class="flex flex-col grow basis-0 gap-2">
-        <label for="">Como nos encontrou:</label>
-        <p-select [options]="opcoesContato" optionLabel="opcao" optionValue="opcao" placeholder="Selecione por onde o cliente veio."/>
-      </div>
+    <div class="card flex flex-col gap-4">
+      <div class="font-bold text-xl">Cadastro de Cliente:</div>
+      <div class="flex flex-wrap basis-0 gap-3">
+        <div class="flex flex-col grow gap-2">
+          <label for="">Nome Completo: <span class="text-red-500"><strong> *</strong></span></label>
+          <input type="text" pInputText placeholder="Digite o nome do cliente.">
+        </div>
+        
+        <div class="flex flex-col grow gap-2">
+          <label for="campo-codigo">Código: <span class="text-red-500"><strong> *</strong></span></label>
+          <input pInputText id="campo-codigo" type="text" placeholder="Digite o código de referência do cliente." />
+        </div>
+        
+        <div class="flex flex-col basis-0 gap-2">
+          <label for="">Celular: <span class="text-red-500"><strong> *</strong></span></label>
+          <p-inputmask mask="(99) 99999-9999" placeholder="(00) 00000-0000" />
+        </div>
+        
+        <div class="flex flex-col grow basis-0 gap-2">
+          <label for="">E-mail: <span class="text-red-500"><strong> *</strong></span></label>
+          <input type="email" pInputText placeholder="Digite o e-mail do cliente.">
+        </div>
+        
+        <div class="flex flex-col grow basis-0 gap-2">
+          <label for="">Como nos encontrou:</label>
+          <p-select [options]="opcoesContato" optionLabel="opcao" optionValue="opcao" placeholder="Selecione por onde o cliente veio."/>
+        </div>
 
       <div class="flex flex-col grow basis-0 gap-2">
         <label for="">Tipo do Cliente: <span class="text-red-500"><strong> *</strong></span></label>
         <p-select [options]="tipo" id="tipo-cliente" optionLabel="tipo" optionValue="tipo" placeholder="Selecione o tipo do cliente." [(ngModel)]="tipoClienteSelecionado"/>
       </div>
     </div>
-
+    
     <div id="mostrar-form" class="flex flex-col gap-4">
       @switch (tipoClienteSelecionado) {
         @case ("Interessado") {
-  <div id="form-interessado" class="card flex flex-col gap-4">
-    <div class="font-bold text-xl">Informações Sobre o Cliente:</div>
-
-    <div class="flex flex-col gap-2">
-      <label for="">O que está Procurando? <span class="text-red-500"><strong> *</strong></span></label>
-      <p-select [options]="tipoImovel" optionLabel="tipo" optionValue="tipo" placeholder="Selecione a opção desejada." [(ngModel)]="tipoSelecionado"/>
-    </div>
-    
-    <div class="text-xl font-bold">Valores:</div>
-    <div class="flex flex-wrap gap-2">
-
-      <div class="flex flex-col grow gap-2">
-        <label for="">Orçamento: <span class="text-red-500"><strong> *</strong></span></label>
-        <p-inputnumber mode="currency" placeholder="Orçamento do cliente." currency="BRL" locale="pt-BR" />
-      </div>
-
-      <div class="flex flex-col grow gap-2">
-        <label for="">Orçamento Mínimo:</label>
-        <p-inputnumber mode="currency" placeholder="Valor mínimo do orçamento do cliente." currency="BRL" locale="pt-BR" />
-      </div>
-      
-      <div class="flex flex-col grow gap-2">
-        <label for="">Orçamento Máximo:</label>
-        <p-inputnumber mode="currency" placeholder="Valor máximo do orçamento do cliente." currency="BRL" locale="pt-BR" />
-      </div>
-    </div>
-
-  <div class="flex flex-col gap-2">
-    <div class="font-bold text-xl">Preferências:</div>
-
-    <div class="flex flex-wrap gap-6">
-      <div class="flex flex-col grow basis-0 gap-2">
-        <label for="campo-quartos">Quantidade de Quartos:</label>
-          <p-inputnumber
-          id="campo-quartos" 
-          placeholder="Digite a quantidade de quartos." />
-        </div>
-
-        <div class="flex flex-col grow basis-0 gap-2">
-          <label for="campo-suites">Sendo Suítes:</label>
-          <p-inputnumber
-          id="campo-suites"
-          placeholder="Digite a quantidade de suítes." />
-        </div>
-
-        <div class="flex flex-col grow basis-0 gap-2">
-          <label for="campo-banheiro">Quantidade de Banheiros:</label>
-          <p-inputnumber
-          id="campo-banheiro" 
-          placeholder="Digite a quantidade de banheiros." />
-        </div>
-
-        <div class="flex flex-col grow basis-0 gap-2">
-          <label for="campo-vagas">Vagas de Garagem:</label>
-          <p-inputnumber 
-          id="campo-vagas"
-          placeholder="Digite a quantidade de vagas de garagem." />
-        </div>
-
-        <div class="flex flex-col grow basis-0 gap-2">
-          <label for="campo-andares">Andares:</label>
+          <div id="form-interessado" class="card flex flex-col gap-4">
+            <div class="font-bold text-xl">Informações Sobre o Cliente:</div>
+            
+            <div class="flex flex-col gap-2">
+              <label for="">O que está Procurando? <span class="text-red-500"><strong> *</strong></span></label>
+              <p-select [options]="tipoImovel" optionLabel="tipo" optionValue="tipo" placeholder="Selecione a opção desejada." [(ngModel)]="tipoSelecionado"/>
+            </div>
+            
+            <div class="text-xl font-bold">Valores:</div>
+            <div class="flex flex-wrap gap-2">
+              
+              <div class="flex flex-col grow gap-2">
+                <label for="">Orçamento: <span class="text-red-500"><strong> *</strong></span></label>
+                <p-inputnumber mode="currency" placeholder="Orçamento do cliente." currency="BRL" locale="pt-BR" />
+              </div>
+              
+              <div class="flex flex-col grow gap-2">
+                <label for="">Orçamento Mínimo:</label>
+                <p-inputnumber mode="currency" placeholder="Valor mínimo do orçamento do cliente." currency="BRL" locale="pt-BR" />
+              </div>
+              
+              <div class="flex flex-col grow gap-2">
+                <label for="">Orçamento Máximo:</label>
+                <p-inputnumber mode="currency" placeholder="Valor máximo do orçamento do cliente." currency="BRL" locale="pt-BR" />
+              </div>
+            </div>
+            
+            <div class="flex flex-col gap-2">
+              <div class="font-bold text-xl">Preferências:</div>
+              
+              <div class="flex flex-wrap gap-6">
+                <div class="flex flex-col grow basis-0 gap-2">
+                  <label for="campo-quartos">Quantidade de Quartos:</label>
+                  <p-inputnumber
+                  id="campo-quartos" 
+                  placeholder="Digite a quantidade de quartos." />
+                </div>
+                
+                <div class="flex flex-col grow basis-0 gap-2">
+                  <label for="campo-suites">Sendo Suítes:</label>
+                  <p-inputnumber
+                  id="campo-suites"
+                  placeholder="Digite a quantidade de suítes." />
+                </div>
+                
+                <div class="flex flex-col grow basis-0 gap-2">
+                  <label for="campo-banheiro">Quantidade de Banheiros:</label>
+                  <p-inputnumber
+                  id="campo-banheiro" 
+                  placeholder="Digite a quantidade de banheiros." />
+                </div>
+                
+                <div class="flex flex-col grow basis-0 gap-2">
+                  <label for="campo-vagas">Vagas de Garagem:</label>
+                  <p-inputnumber 
+                  id="campo-vagas"
+                  placeholder="Digite a quantidade de vagas de garagem." />
+                </div>
+                
+                <div class="flex flex-col grow basis-0 gap-2">
+                  <label for="campo-andares">Andares:</label>
           <p-inputnumber 
           id="campo-andares"
           placeholder="Digite a quantidade de andares." />
         </div>
-
+        
         <div class="flex flex-col grow basis-0 gap-2">
           <label for="campo-salas">Quantidade de Salas:</label>
           <p-inputnumber
@@ -151,7 +154,7 @@ export interface TiposImoveis {
       </div>
     </div>
   </div>
-
+  
   <div class="flex justify-end">
     <p-button
     severity="success"
@@ -159,60 +162,61 @@ export interface TiposImoveis {
     label="Salvar"
     (click)="salvar()" />
   </div>
-        }
-        @case ("Proprietário") {
-           <div class="flex card gap-2 flex-col">
+}
+@case ("Proprietário") {
+  <div class="flex card gap-2 flex-col">
     <div class="flex flex-col grow basis-0 gap-4">
-        <div class="text-xl font-bold">Informações Adicionais:</div>
-
-        <label for="">Imóvel do Responsável:</label>
-        <div class="flex flex-row">
-           <p-select [options]="imoveis" optionLabel="nome" optionValue="nome" placeholder="Selecione o imóvel." fluid />
-           <p-button
-           severity="primary"
-           icon="pi pi-plus" />
-          </div>
-     </div>
-   </div>
-
-  <div class="flex justify-end">
-    <p-button
-    severity="success"
-    icon="pi pi-save"
-    label="Salvar"
-    (click)="salvar()" />
-  </div>
-        }
-        @case ("Locatário") {
-           <div class="flex card gap-2 flex-col">
-    <div class="flex flex-col grow basis-0 gap-4">
-        <div class="text-xl font-bold">Informações Adicionais:</div>
-
-        <label for="">Imóvel do Responsável:</label>
-        <div class="flex flex-row">
-           <p-select [options]="imoveis" optionLabel="nome" optionValue="nome" placeholder="Selecione o imóvel." fluid />
-           <p-button
-           severity="primary"
-           icon="pi pi-plus" />
-          </div>
-     </div>
-   </div>
-
-  <div class="flex justify-end">
-    <p-button
-    severity="success"
-    icon="pi pi-save"
-    label="Salvar"
-    (click)="salvar()" />
-  </div>
-        }
-        @default {
-        }
-      }
+      <div class="text-xl font-bold">Informações Adicionais:</div>
+      
+      <label for="">Imóvel do Responsável:</label>
+      <div class="flex flex-row">
+        <p-select [options]="imoveis" optionLabel="nome" optionValue="nome" placeholder="Selecione o imóvel." fluid />
+        <p-button
+        severity="primary"
+        icon="pi pi-plus" />
+      </div>
     </div>
   </div>
-  `,
-  styles: ``
+  
+    <div class="flex justify-end">
+      <p-button
+      severity="success"
+      icon="pi pi-save"
+      label="Salvar"
+      (click)="salvar()" />
+    </div>
+          }
+          @case ("Locatário") {
+            <div class="flex card gap-2 flex-col">
+              <div class="flex flex-col grow basis-0 gap-4">
+                <div class="text-xl font-bold">Informações Adicionais:</div>
+                
+          <label for="">Imóvel do Responsável:</label>
+          <div class="flex flex-row">
+            <p-select [options]="imoveis" optionLabel="nome" optionValue="nome" placeholder="Selecione o imóvel." fluid />
+            <p-button
+            severity="primary"
+            icon="pi pi-plus" />
+            </div>
+          </div>
+        </div>
+        
+        <div class="flex justify-end">
+          <p-button
+          severity="success"
+          icon="pi pi-save"
+          label="Salvar"
+          (click)="salvar()" />
+        </div>
+      }
+      @default {
+      }
+    }
+    </div>
+  </div>
+</form>
+`,
+styles: ``
 })
 export class ClienteCreate {
   opcoesContato: Opcoes[] | undefined;
@@ -228,7 +232,16 @@ export class ClienteCreate {
   tipoSelecionado: TiposImoveis | undefined;
 
   private readonly formBuilder = inject(FormBuilder);
+  private readonly clienteService = inject(ClienteService);
   private readonly messageService = inject(MessageService);
+
+  clienteForm = this.formBuilder.group({
+    nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(60)]],
+    codigo: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(10)]],
+    celular: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
+    tipoCliente: ['', [Validators.required]],
+  })
 
   constructor(
     private router: Router
