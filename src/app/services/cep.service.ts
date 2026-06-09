d91a140452fa0c1c07ed2379aa59c6b4a1a3217d
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-06-09 10:35:28 -0300

    Modificado nome do modelo de consultas de CEP; criado função para preencher dados após consulta; criado variáveis temporárias para teste de preenchimento de dados.

--- src/app/services/cep.service.ts
+++ src/app/services/cep.service.ts
@@ -1,4 +1,4 @@
-import { ConsultaCepResponse } from '@/models/consulta.cep';
+import { ConsultaCepResponse } from '@/models/consulta.cep.model';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
