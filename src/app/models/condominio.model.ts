9010bb6c641b628f6647d3aceb5d5ae80f0f0b75
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-06-19 10:53:07 -0300

    Criado modelos para editar condomínio e de resposta de condomínios cadastrado.

--- src/app/models/condominio.model.ts
+++ src/app/models/condominio.model.ts
@@ -6,4 +6,25 @@ export interface CriarCondominioRequest {
     bairro: string;
     estado: string;
     cidade: string;
+};
+
+export interface EditarCondominioResquest {
+    nome: string;
+    cep: string;
+    logradouro: string;
+    numero: number;
+    bairro: string;
+    estado: string;
+    cidade: string;
+}
+
+export interface CondominioResponse {
+    id: string;
+    nome: string;
+    cep: string;
+    logradouro: string;
+    numero: number;
+    bairro: string;
+    estado: string;
+    cidade: string;
 }
