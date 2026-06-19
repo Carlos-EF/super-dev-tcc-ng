03ba4765c34a877139a8d629cb37de402fcc3cee
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-06-19 10:56:10 -0300

    Criado função para obter todos os condomínios cadastrados; criado função para obter um condomínio por seu ID.

--- src/app/services/condominio.service.ts
+++ src/app/services/condominio.service.ts
@@ -1,6 +1,8 @@
+import { CondominioResponse } from '@/models/condominio.model';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,17 @@ import { Observable } from 'rxjs';
 export class CondominioService {
   private httpClient = inject(HttpClient);
 
-  getAll(): Observable<>
+  getAll(): Observable<CondominioResponse[]> {
+    const url = `${environment.apiUrl}/condominios`;
+
+    return this.httpClient.get<CondominioResponse[]>(url);
+  }
+
+  getById(
+    id: string
+  ): Observable<CondominioResponse> {
+    const url = `${environment.apiUrl}/condominios/${id}`;
+
+    return this.httpClient.get<CondominioResponse>(url);
+  }
 }
