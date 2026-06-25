03defe9dab6f9346037c0605545f2e394b20b703
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-06-25 09:55:25 -0300

    Criado modelo para editar imóveis.

--- src/app/models/imovel.model.ts
+++ src/app/models/imovel.model.ts
@@ -24,3 +24,29 @@ export interface CriarImovelRequest {
     quantidade_salas: number;
     esta_mobiliado: boolean;
 }
+
+export interface EditarImovelRequest {
+    id_proprietario: string;
+    id_corretor: string;
+    finalidade: string;
+    tipo: string;
+    em_condominio: boolean;
+    condominio: string;
+    cep: string;
+    logradouro: string;
+    numero: number;
+    estado: string;
+    cidade: string;
+    bairro: string;
+    complemento: string;
+    valor: number;
+    valor_condominio: number;
+    iptu: number;
+    quantidade_quartos: number;
+    quantidade_suites: number;
+    quantidade_banheiros: number;
+    quantidade_vagas: number;
+    quantidade_andares: number;
+    quantidade_salas: number;
+    esta_mobiliado: boolean;
+}
