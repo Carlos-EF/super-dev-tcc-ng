4733fca33f49b3b1eece8ced1e9d8ff57553e646
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-06-26 09:10:33 -0300

    Criado arquivo de service de imóveis; criado função para buscar todos os imóveis.

---
+++ src/app/services/imovel.service.ts
@@ -0,0 +1,18 @@
+import { ImovelResponse } from '@/models/imovel.model';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImovelService {
+  private url = `${environment.apiUrl}/imoveis`;
+
+  private httpClient = inject(HttpClient);
+
+  getAll(): Observable<ImovelResponse[]> {
+    return this.httpClient.get<ImovelResponse[]>(this.url);
+  }
+}
