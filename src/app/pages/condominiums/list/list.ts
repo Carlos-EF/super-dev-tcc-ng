cd1a639507e305027f07a1ced8df2d85c120f516
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-07-22 10:56:55 -0300

    Adicionado filtro na tela de listagem; adicionado restante do CSS da listagem de condomínios (filtros).

--- src/app/pages/condominiums/list/list.ts
+++ src/app/pages/condominiums/list/list.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-condominiums-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
