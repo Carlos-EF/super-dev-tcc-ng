b88331c01d86aa7ab771fafcb102b533496e2cdd
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-07-22 08:45:42 -0300

    Criado pasta de componentes; criado componente de topbar.

---
+++ src/app/components/topbar.component/topbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar.component',
+  imports: [],
+  templateUrl: './topbar.component.html',
+  styleUrl: './topbar.component.scss',
+})
+export class TopbarComponent {}
