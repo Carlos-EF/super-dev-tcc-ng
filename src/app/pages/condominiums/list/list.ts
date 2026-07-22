833d5ef5ef4fcf6af4014095ed070c66e576c371
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-07-22 10:15:09 -0300

    Criado componente de condominiums; criado list de condominium.

---
+++ src/app/pages/condominiums/list/list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-condominiums-list',
+  imports: [],
+  templateUrl: './list.html',
+  styleUrl: './list.scss',
+})
+export class CondominiumsList {}
