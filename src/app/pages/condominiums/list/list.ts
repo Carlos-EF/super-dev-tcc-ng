ff8e2acb4768a5fd7744f0e70b2d10ca9621ccfa
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-07-25 09:20:30 -0300

    Criado environment.

--- src/app/pages/condominiums/list/list.ts
+++ src/app/pages/condominiums/list/list.ts
@@ -21,11 +21,12 @@ export class CondominiumsList {
 
   createCondominiumForm = this.formBuilder.group({
     nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(60)]],
-    cep: ['', [Validators.maxLength(9)]],
-    logradouro: ['', [Validators.minLength(3), Validators.maxLength(60)]],
+    cep: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+    logradouro: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(60)]],
     numero: [this.formBuilder.control<number>(0), Validators.required],
     bairro: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
-    cidade_uf: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]]
+    uf: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(2)]],
+    cidade: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]]
   });
 
   openCreateModal() {
