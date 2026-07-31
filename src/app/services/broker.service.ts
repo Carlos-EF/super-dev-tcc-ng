23be7ff4e2cb6aba3976d711586606c1f7d9352b
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-07-31 09:56:35 -0300

    Criado rota para buscar todos.

---
+++ src/app/services/broker.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { environment } from '../../environments/environments';
+import { Observable } from 'rxjs';
+import { BrokerFilters, PaginatedBrokerResponse } from '../models/broker.model';
+
+@Service()
+export class BrokerService {
+    private httpClient = inject(HttpClient);
+
+    private url = `${environment.apiUrl}/brokers`;
+
+    getAll(
+        filters?: BrokerFilters,
+        pagina: number = 1,
+        porPagina: number = 10
+    ): Observable<PaginatedBrokerResponse> {
+        let params = new HttpParams().
+        set('pagina', pagina.toString())
+        .set('por_pagina',porPagina.toString());
+        
+        if (filters?.busca) {
+            params = params.set('busca', filters.busca);
+        }
+
+        return this.httpClient.get<PaginatedBrokerResponse>(
+            this.url,
+            { params }
+        );
+    };
+}
