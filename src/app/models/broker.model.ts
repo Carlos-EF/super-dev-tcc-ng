4779fade8ee6512c9265fe778e3d10cc5d572b95
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-07-31 09:52:32 -0300

    Criado modelo de response.

--- src/app/models/broker.model.ts
+++ src/app/models/broker.model.ts
@@ -18,3 +18,17 @@ export interface EditBrokerRequest {
     rg: string | null;
     cpf: string | null
 }
+
+export interface BrokerResponse {
+    id: string;
+    nome: string;
+    codigo: string;
+    creci: string;
+    numero: string;
+    email: string;
+    data_nascimento: string | null;
+    rg: string | null;
+    cpf: string | null
+    criado_em: Date;
+    alterado_em: Date;
+}
