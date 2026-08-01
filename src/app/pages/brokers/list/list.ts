a3422ad8b98f818eec06383acf06fdd475c78ce8
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-01 08:38:40 -0300

    Adicionado variaveis padrão da página.

--- src/app/pages/brokers/list/list.ts
+++ src/app/pages/brokers/list/list.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, model } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ToastService } from '../../../services/toast.service';
+import { FormBuilder } from '@angular/forms';
+import { BrokerService } from '../../../services/broker.service';
+import { SortType } from '../../../types/sort.types';
+import { BrokerFilters } from '../../../models/broker.model';
 
 @Component({
   selector: 'app-list',
@@ -6,4 +12,24 @@ import { Component } from '@angular/core';
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
-export class BrokersList {}
+export class BrokersList {
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly brokerService = inject(BrokerService);
+  private readonly toastService = inject(ToastService);
+
+  confirmModal: boolean = false;
+
+  busca = new Subject<string>();
+
+  openModal: boolean = false;
+
+  isEditMode: boolean = false;
+
+  perPage = model(10);
+
+  page = model(1);
+
+  sortDirection: SortType = 'asc';
+
+  filters: BrokerFilters = {};
+}
