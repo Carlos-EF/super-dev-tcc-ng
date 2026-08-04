967f5d7d9e6e3685953169009ad80f14160799fc
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-04 09:17:52 -0300

    Criado modelo para criação.

--- src/app/models/clients.model.ts
+++ src/app/models/clients.model.ts
@@ -1,10 +1,20 @@
 import { ClientsTypes } from "../types/clients.types";
 import { ContactTypes } from "../types/contact.types";
+import { FinalityTypes } from "../types/finality.types";
+import { PropertyTypes } from "../types/property.types";
 
 export interface CreateClientRequest {
     nome: string;
+    codigo: string;
     numero: string;
     email: string;
     tipo: ClientsTypes;
     como_encontrou: ContactTypes;
+}
+
+export interface CreateInterestRequest {
+    cliente_id: string;
+    procura: PropertyTypes;
+    finalidade: FinalityTypes;
+    preferencia: string;
 }
