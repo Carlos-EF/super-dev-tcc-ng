bafb9c8afb6c1b5dd754f48a60cfd473919ca2a0
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-07 09:50:02 -0300

    Começado a adicionar variaveis necessárias para página.

--- src/app/pages/clients/list/list.ts
+++ src/app/pages/clients/list/list.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, model } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastService } from '../../../services/toast.service';
+import { ClientsService } from '../../../services/clients.service';
+import { Subject } from 'rxjs';
+import { ClientsFilters, PaginatedClientResponse } from '../../../models/clients.model';
 
 @Component({
   selector: 'app-list',
@@ -11,5 +15,31 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './list.scss',
 })
 export class ClientsList {
-  
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly clientService = inject(ClientsService);
+  private readonly toastService = inject(ToastService);
+
+  confirmModal: boolean = false;
+
+  busca = new Subject<string>();
+
+  openModal: boolean = false;
+
+  isEditMode: boolean = false;
+
+  perPage = model(10);
+
+  page = model(1);
+
+  filters: ClientsFilters = {};
+
+  clients = model<PaginatedClientResponse>(
+    {
+      clientes: [],
+      pagina: 1,
+      por_pagina: 10,
+      total: 0,
+      total_paginas: 0
+    }
+  )
 }
