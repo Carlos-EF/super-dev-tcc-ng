54fadc9f7ccd7f5ede80caca5d134f140f3a0a23
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-15 09:51:24 -0300

    Criado rotas para criação de cada tipo de imóvel.

--- src/app/services/propertys.service.ts
+++ src/app/services/propertys.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { environment } from '../../environments/environments';
 import { Observable } from 'rxjs';
-import { CompletePropertyResponse, CreatePropertyRequest, PaginatedPropertyResponse } from '../models/property.model';
+import { ApartmentResponse, CompletePropertyResponse, CreateApartmentRequest, CreateHouseRequest, CreateLandRequest, CreatePropertyRequest, HouseResponse, LandResponse, PaginatedPropertyResponse } from '../models/property.model';
 
 @Service()
 export class PropertysService {
@@ -36,5 +36,32 @@ export class PropertysService {
         form: CreatePropertyRequest
     ): Observable<CompletePropertyResponse> {
         return this.httpClient.post<CompletePropertyResponse>(this.url, form);
+    };
+
+    createHouse(
+        id: string,
+        form: CreateHouseRequest
+    ): Observable<HouseResponse> {
+        const urlWithId = `${this.url}/${id}/house`;
+
+        return this.httpClient.post<HouseResponse>(urlWithId, form);
+    };
+
+    createApartment(
+        id: string,
+        form: CreateApartmentRequest
+    ): Observable<ApartmentResponse> {
+        const urlWithId = `${this.url}/${id}/apartment`;
+
+        return this.httpClient.post<ApartmentResponse>(urlWithId, form);
+    };
+
+    createLand(
+        id: string,
+        form: CreateLandRequest
+    ): Observable<LandResponse> {
+        const urlWithId = `${this.url}/${id}/land`;
+
+        return this.httpClient.post<LandResponse>(urlWithId, form);
     };
 }
