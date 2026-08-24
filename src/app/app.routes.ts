8f04f1db97dfb034eed29ab95a074749aad84733
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-24 15:40:47 -0300

    Criado guards para proteger login.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,43 +7,58 @@ import { ListProperty } from './pages/propertys/list/list';
 import { CreateProperty } from './pages/propertys/create/create';
 import { EditProperty } from './pages/propertys/edit/edit';
 import { Auth } from './pages/auth/auth';
+import { authGuard } from './core/guards/auth.guard';
+import { guestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
     {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full'
-    },
-    {
         path: 'login',
+        canActivate: [guestGuard],
         component: Auth
     },
     {
-        path: 'home',
-        component: Home
+        path: '',
+        canActivate: [authGuard],
+        children: [
+
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'home'
+            },
+
+            {
+                path: 'home',
+                component: Home
+            },
+            {
+                path: 'condominiums/list',
+                component: ListCondominiums
+            },
+            {
+                path: 'brokers/list',
+                component: ListBrokers
+            },
+            {
+                path: 'clients/list',
+                component: ListClients
+            },
+            {
+                path: 'propertys/list',
+                component: ListProperty
+            },
+            {
+                path: 'propertys/create',
+                component: CreateProperty
+            },
+            {
+                path: 'propertys/:id/edit',
+                component: EditProperty
+            }
+        ]
     },
     {
-        path:'condominiums/list',
-        component: ListCondominiums
-    },
-    {
-        path: 'brokers/list',
-        component: ListBrokers
-    },
-    {
-        path:'clients/list',
-        component: ListClients
-    },
-    {
-        path: 'propertys/list',
-        component: ListProperty
-    },
-    {
-        path: 'propertys/create',
-        component: CreateProperty
-    },
-    {
-        path: 'propertys/edit/:id',
-        component: EditProperty
-    },
+        path: '**',
+        redirectTo: 'inicio'
+    }
 ];
---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,37 @@
+import { inject } from '@angular/core';
+import {
+    CanActivateFn,
+    Router
+} from '@angular/router';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environments';
+
+
+export const authGuard: CanActivateFn = async () => {
+    const supabase: SupabaseClient =
+        createClient(
+            environment.supabaseUrl,
+            environment.supabaseKey
+        );
+
+
+    const router =
+        inject(Router);
+
+    const {
+        data,
+        error
+    } =
+        await supabase.auth.getSession();
+
+    if (
+        error ||
+        !data.session
+    ) {
+        return router.createUrlTree([
+            '/login'
+        ]);
+    }
+
+    return true;
+};
