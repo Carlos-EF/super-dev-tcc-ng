c01432b8558cd8ca2c4d587fc86bf86c65b8666b
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-24 09:02:38 -0300

    Adicionado imports e variaveis necessárias.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, AfterViewInit, ElementRef, OnDestroy, ViewChild, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Chart, ChartConfiguration, registerables } from 'chart.js';
+import { PropertysService } from '../../services/propertys.service';
+import { ClientsService } from '../../services/clients.service';
+import { BrokerService } from '../../services/broker.service';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements OnDestroy {
+  private readonly propertyService = inject(PropertysService);
+  private readonly clientsService = inject(ClientsService);
+  private readonly brokerService = inject(BrokerService);
+
+  private propertyTypeChart?: Chart;
+  private clientTypeChart?: Chart;
+  private propertyValueChart?: Chart;
+
+  ngOnDestroy(): void {
+
+    this.propertyTypeChart?.destroy();
+
+    this.clientTypeChart?.destroy();
+
+    this.propertyValueChart?.destroy();
+  }
+}
