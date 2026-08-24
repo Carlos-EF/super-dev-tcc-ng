c5a8cbc2f9fdfc15877fa4d625e073860bd4134d
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-24 13:43:18 -0300

    Adicionado variaveis e componentes necessárias para login.

--- src/app/pages/auth/auth.ts
+++ src/app/pages/auth/auth.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environments';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './auth.html',
   styleUrl: './auth.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Auth {}
+export class Auth {
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly router = inject(Router);
+
+  private readonly supabase: SupabaseClient =
+    createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey,
+    );
+
+  readonly loading = signal(false);
+
+  readonly showPassword = signal(false);
+
+  readonly errorMessage = signal<string | null>(null);
+
+  readonly successMessage = signal<string | null>(null);
+}
