b77d4a9b22974eb1f0fdd9312771fbaa5edd4a0d
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-24 09:05:05 -0300

    Adicionado mais variaveis nexessárias.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,6 +4,7 @@ import { Chart, ChartConfiguration, registerables } from 'chart.js';
 import { PropertysService } from '../../services/propertys.service';
 import { ClientsService } from '../../services/clients.service';
 import { BrokerService } from '../../services/broker.service';
+import { PropertyTypeStats, PropertyValueStats } from '../../models/home.model';
 
 Chart.register(...registerables);
 
@@ -25,12 +26,55 @@ export class Home implements OnDestroy {
   private clientTypeChart?: Chart;
   private propertyValueChart?: Chart;
 
-  ngOnDestroy(): void {
+  updatedAt = '';
+  totalProperties = 0;
+  totalClients = 0;
+  totalBrokers = 0;
+  saleProperties = 0;
+  rentalProperties = 0;
 
+  propertyTypeStats: PropertyTypeStats = {
+    apartamento: 0,
+    casa: 0,
+    terreno: 0,
+    outros: 0
+  };
+
+  private propertyValueStats: Record<
+    string,
+    PropertyValueStats
+  > = {
+      Apartamento: {
+        min: 0,
+        average: 0,
+        max: 0
+      },
+      Casa: {
+        min: 0,
+        average: 0,
+        max: 0
+      },
+      Terreno: {
+        min: 0,
+        average: 0,
+        max: 0
+      }
+    };
+
+  @ViewChild('propertyTypeChart')
+  propertyTypeChartRef?: ElementRef<HTMLCanvasElement>;
+
+  @ViewChild('clientTypeChart')
+  clientTypeChartRef?: ElementRef<HTMLCanvasElement>;
+
+  @ViewChild('propertyValueChart')
+  propertyValueChartRef?: ElementRef<HTMLCanvasElement>;
+
+  ngOnDestroy(): void {
     this.propertyTypeChart?.destroy();
 
     this.clientTypeChart?.destroy();
 
     this.propertyValueChart?.destroy();
-  }
+  };
 }
