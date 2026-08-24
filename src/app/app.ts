ebfce4fe0b594274d6817e40de0c9a7ceac7d878
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-24 15:16:31 -0300

    Corrigido topbar aparecendo o tempo todo; alterado login.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { Topbar } from "./components/topbar/topbar.component";
 import { Sidebar } from "./components/sidebar/sidebar";
 import { Toast } from './components/toast/toast';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { environment } from '../environments/environments';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,51 @@ import { Toast } from './components/toast/toast';
     Topbar,
     Sidebar,
     Toast
-],
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('tcc');
+
+  private readonly supabase: SupabaseClient =
+    createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+
+  readonly isAuthenticated = signal(false);
+
+  constructor() {
+    this.loadSession();
+
+    this.supabase.auth.onAuthStateChange(
+      (_event, session) => {
+
+        this.isAuthenticated.set(
+          !!session
+        );
+
+      }
+    );
+  }
+
+
+  private async loadSession(): Promise<void> {
+    const {
+      data,
+      error
+    } = await this.supabase.auth.getSession();
+
+    if (error) {
+
+      this.isAuthenticated.set(false);
+
+      return;
+    }
+
+    this.isAuthenticated.set(
+      !!data.session
+    );
+  };
 }
