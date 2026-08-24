f8f838a89db2b69f54c76b8fe5943c542febad16
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2026-08-24 08:56:30 -0300

    Criado modelos para página home (respostas de tipos).

---
+++ src/app/models/home.model.ts
@@ -0,0 +1,13 @@
+export interface PropertyTypeStats {
+    apartamento: number;
+    casa: number;
+    terreno: number;
+    outros: number;
+}
+
+
+export interface PropertyValueStats {
+    min: number;
+    average: number;
+    max: number;
+}
